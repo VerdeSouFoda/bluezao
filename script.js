9a501c7c29c1c42119e3452f28124ba42d551dd6
VerdeSouFoda <136626834+VerdeSouFoda@users.noreply.github.com>
2023-06-14 14:24:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
+var currentAudio = null;
+
 document.querySelector(".bloqueado").addEventListener("click", function () {
-    new Audio("sounds/bloqueado-sound.mp3").play();
+    if (currentAudio) {
+        currentAudio.pause();
+    }
+    currentAudio =  new Audio("sounds/bloqueado-sound.mp3")
+    currentAudio.play();
 });
 
 document.querySelector(".porta-fantasma").addEventListener("click", function () {
-    var audioPortaFantasma = ("sounds/porta-fantasma.mp3");
-    new Audio(audioPortaFantasma).play();
+    if (currentAudio) {
+        currentAudio.pause();
+    }
+    currentAudio = new Audio("sounds/porta-fantasma.mp3");
+    currentAudio.play();
 });
 
 document.querySelector(".gemidao").addEventListener("click", function () {
-    new Audio("sounds/gemidao.mp3").play();
+    if (currentAudio) {
+        currentAudio.pause();
+    }
+    currentAudio =new Audio("sounds/gemidao.mp3");
+    currentAudio.play();
 })
 
 document.querySelector(".cu").addEventListener("click", function () {
-    new Audio("sounds/eu-quero-dar-o-cu.mp3").play();
+    if (currentAudio) {
+        currentAudio.pause();
+    }
+    currentAudio = new Audio("sounds/eu-quero-dar-o-cu.mp3");
+    currentAudio.play();
 })
 
 document.querySelector(".picpay").addEventListener("click", function () {
-    new Audio("sounds/picpay.mp3").play();
+    if (currentAudio) {
+        currentAudio.pause();
+    }
+    currentAudio = new Audio("sounds/picpay.mp3");
+    currentAudio.play();
 })
 
 document.querySelector(".tosse").addEventListener("click", function () {
-    new Audio("sounds/tosse.mp3").play();
+    if (currentAudio) {
+        currentAudio.pause();
+    }
+    currentAudio = new Audio("sounds/tosse.mp3");
+    currentAudio.play();
 })
